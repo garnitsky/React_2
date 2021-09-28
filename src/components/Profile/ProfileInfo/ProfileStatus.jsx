3ce779726c1d8fb2281323266ec7707671545a1a
Oleksandr Harnytskyi <garnitsky@gmail.com>
2021-09-28 23:55:08 +0300

    75-1

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -27,12 +27,20 @@ class ProfileStatus extends React.Component {
         })
     };
 
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            });
+        }
+    }
+
     render() {
         return (
             <div>
                 {!this.state.editMode ?
                     <div>
-                        <span onClick={this.activateEditMode}>{this.props.status}</span>
+                        <span onDoubleClick={this.activateEditMode}>{this.props.status || "--------"}</span>
                     </div>
                     :
                     <div>
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,7 +1,31 @@
 
 
-const Login = (props) => {
-    return <h1>Login</h1>
+const LoginForm = (props) => {
+    return (
+        <form>
+            <div>
+                <input placeholder={"Login"} />
+            </div>
+            <div>
+                <input placeholder={"Password"} />
+
+            </div>
+            <div>
+                <input type={"checkbox"} /> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+
+        </form>
+    )
 };
+
+const Login = () => {
+    return <div>
+        <h1>Login</h1>
+        <LoginForm />
+    </div>
+}
 
 export default Login;
